9a1dba3e0ce19a01d561007c1e29bc3f865b5c68
MihajlovskaE <mihajlovskaelenaa@gmail.com>
2020-06-09 00:44:54 +0200

    portfolio

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ for(let i = 0; i < divs.length; i++) {
 
 
 
+
 let mainNavLinks = document.querySelectorAll("ul li a");
 let mainSections = document.querySelectorAll("#mainBody section");
 
@@ -60,7 +61,7 @@ function openCloseMenu(){
   }
 }
 
-let progress = document.querySelectorAll(".progress");
+/*let progress = document.querySelectorAll(".progress");
 progress.addEventListener("focusin", function move(){
   let i=0;
   if (i == 0) {
@@ -78,7 +79,7 @@ progress.addEventListener("focusin", function move(){
       }
     }
   }
-});
+});*/
 
 
 
@@ -155,4 +156,46 @@ contactBTn.addEventListener("click", function(){
 
   })
 
- 
+  filterSelection("all")
+  function filterSelection(c) {
+    var x, i;
+    x = document.getElementsByClassName("column");
+    if (c == "all") c = "";
+    for (i = 0; i < x.length; i++) {
+      w3RemoveClass(x[i], "show");
+      if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    }
+  }
+  
+  function w3AddClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+      if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+    }
+  }
+  
+  function w3RemoveClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+      while (arr1.indexOf(arr2[i]) > -1) {
+        arr1.splice(arr1.indexOf(arr2[i]), 1);     
+      }
+    }
+    element.className = arr1.join(" ");
+  }
+  
+  
+  // Add active class to the current button (highlight it)
+  var btnContainer = document.getElementById("myBtnContainer");
+  var btns = btnContainer.getElementsByClassName("btn");
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function(){
+      var current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    });
+  }
